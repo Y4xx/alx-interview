31c428e8052659b411b73965a2b9e28c8ef25d75
Yassine OUJAMA <yassine.oujama@gmail.com>
2025-05-29 19:58:16 +0100

    add 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// script comment
+
+const request = require("request");
+const API_URL = "https://swapi-api.alx-tools.com/api";
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charUrl = JSON.parse(body).characters;
+    const charactersName = charUrl.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        }),
+    );
+
+    Promise.all(charactersName)
+      .then((names) => console.log(names.join("\n")))
+      .catch((allErr) => console.log(allErr));
+  });
+}
